f2a17d94b7542f817f23b806665007e0335eab9b
ratan hegde <ratanhegde08@gmail.com>
2020-09-27 15:27:43 +0530

    add fractional knapsack

---
+++ Fractional_Knapsack.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Item{
+    int val;
+    int w;
+};
+
+bool cmp(Item i1,Item i2){
+    double r1= (double)i1.val/i1.w;
+    double r2=(double)i2.val/i2.w;
+    return r1>r2;
+}
+
+int fractionalKnapsack(Item items[],int n,int W){
+    sort(items,items+n,cmp);
+
+    int rem=W;
+    double ans=0;
+    for(int i=0;i<n;i++){
+        if(items[i].w<=rem){
+            ans+= items[i].val;
+            rem-=items[i].w;
+        }
+        else{
+            ans+= (double)(rem* items[i].val)/(double)items[i].w;
+            rem=0;break;
+        }
+    }
+    return ans;
+
+}
+
+int main(){
+    int W = 50;
+    int value[]={60,100,120};
+    int wt[]={10,20,30};
+    int n=3;
+    Item items[n];
+    for(int i=0;i<n;i++){
+        items[i].val=value[i];
+        items[i].w=wt[i];
+    }
+    int ans= fractionalKnapsack(items,n,W);
+    cout<<"Maximum Value : "<<ans;
+}
