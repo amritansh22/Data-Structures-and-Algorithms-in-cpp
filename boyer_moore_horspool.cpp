2dd4710058f1e7b86a701f6520a933f831fab054
David Kanekanian <dkanekanian1@gmail.com>
2020-10-01 19:51:03 +0100

    Add compile time evaluation

--- boyer_moore_horspool.cpp
+++ boyer_moore_horspool.cpp
@@ -5,15 +5,19 @@
 
 
 struct SearchPattern {
-    std::string_view needle;
-    std::array<char, 256> bad_char_table;
-    SearchPattern(const std::string_view needle) : needle(needle) {
+    constexpr static std::size_t num_chars{ 256 };
+    const std::string_view needle;
+    std::array<char, num_chars> bad_char_table{};
+
+    constexpr SearchPattern(const std::string_view needle) noexcept
+        : needle(needle) {
         // compute bad char table
-        bad_char_table = {};
 
         // Rule 1
         // Characters not in the needle jump the length of the needle.
-        bad_char_table.fill(needle.length());
+        for (std::size_t i = 0; i < num_chars; i++) {
+            bad_char_table[i] = needle.length();
+        }
 
 
         // Rule 2
@@ -26,7 +30,7 @@ struct SearchPattern {
 };
 
 /** Returns index of first occurrance of pattern in haystack. */
-std::optional<std::size_t> boyer_moore_horspool(SearchPattern pattern, std::string_view haystack) {
+constexpr std::optional<std::size_t> boyer_moore_horspool(SearchPattern pattern, std::string_view haystack) {
     for (std::size_t h = 0; h < haystack.length() - pattern.needle.length() + 1;) {
         // Search this placement of the needle.
         bool found = true;
@@ -53,9 +57,11 @@ std::optional<std::size_t> boyer_moore_horspool(SearchPattern pattern, std::stri
 }
 
 int main() {
-    auto needle = SearchPattern{ "abc" };
-    auto haystack = "aa abc ddef";
-    auto index = boyer_moore_horspool(needle, haystack);
+    constexpr SearchPattern needle{ "abc" };
+    constexpr auto haystack = "aa abc ddef";
+    constexpr auto index = boyer_moore_horspool(needle, haystack);
+    static_assert(index == 3, "index is supposed to be 3!");
+
     if (index) {
         std::cout << "found at index " << index.value() << '\n' << std::flush;
     } else {
