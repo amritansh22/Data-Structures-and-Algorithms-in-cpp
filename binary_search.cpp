3963a1616980b905e48e20ea93964d8b6000ef5f
Guna Gokul <gokul.parupalli4679@gmail.com>
2020-10-01 19:17:12 +0530

    Update binary_search.cpp

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,49 +1,42 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-void bsearch(int a[],int n,int key)
-{
-  sort(a,a+n);
-  int start,end,mid;
-  
-  start = 0;
-  end = n-1;
-  bool found = false;
-  while(start<=end)
-  {
-    mid = (start+end)/2;
-    if(a[mid]==key)
-    {
-      found = true;
-      break;
-    }
-    else
-    if(a[mid]>key)
-      end = mid-1;
-    else
-      start = mid+1;
-  }
-  if(found)
-  cout<<key<<" found at index "<<mid<<endl;
-  else
-  cout<<key<<" not found\n";
-}
 
 int main()
 {
-  int n,k,i;
-  cout<<"Enter the number of elements in the array:- ";
-  cin>>n;
-  int arr[n];
- 
-  cout<<"Enter "<<n<<" elements\n";
-  for(i=0;i<n;++i)
-  cin>>arr[i];
-     
-  cout<<"Enter the element to be searched:- ";
-  cin>>k;
-  
-  bsearch(arr,n,k);
-  return 0; 
-}
+	int count, i, arr[30], num, first, last, middle;
+	cout<<"how many elements would you like to enter?:"; 
+        cin>>count;
 
+	for (i=0; i<count; i++)
+	{
+		cout<<"Enter number "<<(i+1)<<": "; 
+                cin>>arr[i];
+	}
+	cout<<"Enter the number that you want to search:"; 
+        cin>>num;
+	first = 0;
+	last = count-1;
+	middle = (first+last)/2;
+	while (first <= last)
+	{
+	   if(arr[middle] < num)
+	   {
+		first = middle + 1;
+
+	   }
+	   else if(arr[middle] == num)
+	   {
+		cout<<num<<" found in the array at the location "<<middle+1<<"\n"; 
+                break; 
+           } 
+           else { 
+                last = middle - 1; 
+           } 
+           middle = (first + last)/2; 
+        } 
+        if(first > last)
+	{
+	   cout<<num<<" not found in the array";
+	}
+	return 0;
+}
