e542dbabc0179957864194abf1822f97dbe4ba65
Ruturaj <ruturaj.7802@gmail.com>
2021-10-21 20:43:25 +0530

    Added Linear Search in C++ language

---
+++ SearchingAlgorithms/linear_search.cpp
@@ -0,0 +1,58 @@
+// C++ program for linear search
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void search(vector<int> arr, int search_Element)
+{
+    int left = 0;
+    int length = arr.size();
+    int position = -1;
+      int right = length - 1;
+       
+    // Run loop from 0 to right
+    for(left = 0; left <= right;)
+    {
+         
+        // If search_element is found with
+        // left variable
+        if (arr[left] == search_Element)
+        {
+             
+            position = left;
+            cout << "Element found in Array at "
+                 << position + 1 << " Position with "
+                 << left + 1 << " Attempt";
+                
+            break;
+        }
+       
+        // If search_element is found with
+        // right variable
+        if (arr[right] == search_Element)
+        {
+            position = right;
+            cout << "Element found in Array at "
+                 << position + 1 << " Position with "
+                 << length - right << " Attempt";
+                
+            break;
+        }
+        left++;
+        right--;
+    }
+ 
+    // If element not found
+    if (position == -1)
+        cout << "Not found in Array with "
+             << left << " Attempt";
+}
+ 
+// Driver code
+int main()
+{
+    vector<int> arr{ 1, 2, 3, 4, 5 };
+    int search_element = 5;
+     
+    // Function call
+    search(arr, search_element);
+}
